30d22e59e756172cf1aefe65db319763e08043f7
JuanGR-FI <juan60430@hotmail.com>
2023-09-12 19:11:04 -0600

    Initial commit

---
+++ app/src/main/java/com/jacgr/filmsrf/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jacgr.filmsrf.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jacgr.filmsrf.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
